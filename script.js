ee6227e3eec8e5bbacb138cdb7fb1619b9e67a2e
Rushikesh Dhanawade <rushikesh26699@gmail.com>
2025-01-16 21:39:45 +0530

    locomotive applied

--- script.js
+++ script.js
@@ -1,3 +1,45 @@
+
+function locomotiveAnimation(){
+  gsap.registerPlugin(ScrollTrigger);
+
+// Using Locomotive Scroll from Locomotive https://github.com/locomotivemtl/locomotive-scroll
+
+const locoScroll = new LocomotiveScroll({
+  el: document.querySelector("#main"),
+  smooth: true
+});
+// each time Locomotive Scroll updates, tell ScrollTrigger to update too (sync positioning)
+locoScroll.on("scroll", ScrollTrigger.update);
+
+// tell ScrollTrigger to use these proxy methods for the "#main" element since Locomotive Scroll is hijacking things
+ScrollTrigger.scrollerProxy("#main", {
+  scrollTop(value) {
+    return arguments.length ? locoScroll.scrollTo(value, 0, 0) : locoScroll.scroll.instance.scroll.y;
+  }, // we don't have to define a scrollLeft because we're only scrolling vertically.
+  getBoundingClientRect() {
+    return {top: 0, left: 0, width: window.innerWidth, height: window.innerHeight};
+  },
+  // LocomotiveScroll handles things completely differently on mobile devices - it doesn't even transform the container at all! So to get the correct behavior and avoid jitters, we should pin things with position: fixed on mobile. We sense it by checking to see if there's a transform applied to the container (the LocomotiveScroll-controlled element).
+  pinType: document.querySelector("#main").style.transform ? "transform" : "fixed"
+});
+
+
+
+
+tl.from(".purple p", {scale: 0.3, rotation:45, autoAlpha: 0, ease: "power2"})
+  .from(".line-3", {scaleX: 0, transformOrigin: "left center", ease: "none"}, 0)
+  .to(".purple", {backgroundColor: "#28a92b"}, 0);
+
+
+
+// each time the window updates, we should refresh ScrollTrigger and then update LocomotiveScroll. 
+ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
+
+// after everything is set up, refresh() ScrollTrigger and update LocomotiveScroll because padding may have been added for pinning, etc.
+ScrollTrigger.refresh();
+
+}
+
 function loadingAnimation() {
   var tl = gsap.timeline();
 
@@ -29,33 +71,33 @@ function loadingAnimation() {
   });
 
   tl.to("#loader", {
-    opacity: 0,
-    duration: 0.2,
-    delay: 1,
+    opacity:0,
+    duration: 0.5,
+    delay: 2.6,
     display:"none"
   });
 
   tl.from("#page1", {
-    delay: 0.2,
+    // delay: 0.1,
     y: 1200,
-    opacity: 0,
-    duration: 0.1,
+    // opacity: 0,
+    duration: 0.2,
     ease: Power4,
   });
 
-  // tl.to("loader", {
-  //   display: "none",
+  tl.to("loader", {
+    display: "none",
     
-  // });
-  tl.from(".nav",{
+  });
+  tl.from("#nav",{
      opacity:0
   })
   tl.from("#hero1 h1,#hero2 h1,#hero3 h2,#hero4 h1",{
-    y:120,
+    y:140,
     stagger:0.1
   })
 }
-loadingAnimation();
+
 
 function cursorAnimation() {
   document.addEventListener("mousemove", function (dets) {
@@ -67,4 +109,6 @@ function cursorAnimation() {
 
   Shery.makeMagnet("#nav-part2 h4", {});
 }
-cursorAnimation()
+// cursorAnimation()
+loadingAnimation();
+locomotiveAnimation()
